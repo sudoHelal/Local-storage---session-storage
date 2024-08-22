df31cf54fe28ff1fce147d37e30e04e4526d01eb
sudohelal <helaluddin40@outlook.com>
2024-08-22 11:51:21 +0600

    update notation.js

---
+++ notation.js
@@ -0,0 +1,15 @@
+const person = {
+  name: "Rifat Al Raju",
+  proffession: "Teacher",
+  age: 30,
+  "passing-year": 2016,
+};
+// dot notation
+const passingYear = person["passing-year"];
+// console.log(passingYear);
+// brackets or box notation
+const passingYear1 = person["passing-year"];
+// console.log(passingYear1);
+const name1 = 'name';
+const personName = person[name1];
+console.log(personName);
